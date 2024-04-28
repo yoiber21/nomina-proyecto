41b66990cc219587808d41676897df77f4098d91
Yoiber Beitar <yoiber.beitar@mercadolibre.com.co>
2024-04-28 16:00:52 -0500

    Scaffolding inicial

---
+++ src/main/java/org/example/nominatercercorte/NominaTercerCorteApplication.java
@@ -0,0 +1,13 @@
+package org.example.nominatercercorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominaTercerCorteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NominaTercerCorteApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/nominatercercorte/NominaTercerCorteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nominatercercorte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominaTercerCorteApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
