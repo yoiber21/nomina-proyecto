46ba3f16b57a87431639e1c0934e684bec44c19d
Yoiber Beitar <yoiber.beitar@mercadolibre.com.co>
2024-05-15 10:46:51 -0500

    Configuracion

--- src/main/java/org/example/nominatercercorte/service/RecruitmentService.java
+++ src/main/java/org/example/nominatercercorte/service/RecruitmentService.java
@@ -3,6 +3,7 @@ package org.example.nominatercercorte.service;
 import org.example.nominatercercorte.exception.NotFoundException;
 import org.example.nominatercercorte.model.Contract;
 import org.example.nominatercercorte.model.UserEntity;
+import org.example.nominatercercorte.repository.IUserEmployeeRepository;
 import org.example.nominatercercorte.repository.RecruitmentRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -12,9 +13,11 @@ import java.util.Optional;
 public class RecruitmentService {
 
   private final RecruitmentRepository contractRepository;
+  private final IUserEmployeeRepository userEmployeeRepository;
 
-  public RecruitmentService(RecruitmentRepository contractRepository) {
+  public RecruitmentService(RecruitmentRepository contractRepository, IUserEmployeeRepository userEmployeeRepository) {
     this.contractRepository = contractRepository;
+    this.userEmployeeRepository = userEmployeeRepository;
   }
 
   public List<Contract> getAllContracts() {
@@ -25,12 +28,12 @@ public class RecruitmentService {
     return contractRepository.findById(id);
   }
 
-public Contract createContract(Contract contract) {
-    UserEntity user = contractRepository.findById(contract.getUser().getId())
-        .orElseThrow(() -> new NotFoundException("User not found with id: " + contract.getUser().getId())).getUser();
+  public Contract createContract(Contract contract) {
+    UserEntity user = userEmployeeRepository.findById(contract.getUser().getId())
+        .orElseThrow(() -> new NotFoundException("User not found with id: " + contract.getUser().getId()));
     contract.setUser(user);
     return contractRepository.save(contract);
-}
+  }
 
   public Contract updateContract(Long id, Contract contract) {
 
