82d0dc92be69c434dd8b3854118d4ff41020b182
Yoiber Beitar <yoiber.beitar@mercadolibre.com.co>
2024-05-15 14:13:17 -0500

    Ajustes del cron

--- src/main/java/org/example/nominatercercorte/config/PayrollScheduler.java
+++ src/main/java/org/example/nominatercercorte/config/PayrollScheduler.java
@@ -2,10 +2,12 @@ package org.example.nominatercercorte.config;
 
 import com.itextpdf.text.DocumentException;
 import java.io.IOException;
+import lombok.extern.log4j.Log4j2;
 import org.example.nominatercercorte.service.PayrollService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Log4j2
 @Component
 public class PayrollScheduler {
 
@@ -17,6 +19,7 @@ public class PayrollScheduler {
 
   @Scheduled(cron = "0 0 18 * * ?", zone = "America/Bogota")
   public void executePayroll() throws DocumentException, IOException {
+    log.info("Executing payroll...");
     payrollService.executePayroll();
   }
 }
